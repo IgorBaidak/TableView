077cd649459583fd7074f07d52c213e8a094234e
IgorBaidak <igorbaidak@gmail.com>
2022-08-14 23:45:17 +0300

    commit
    commit

---
+++ TableView/TableView/Users.swift
@@ -0,0 +1,23 @@
+//
+//  Users.swift
+//  TableView
+//
+//  Created by Igor Baidak on 14.08.22.
+//
+
+import Foundation
+
+
+//class Users: UsersProtocol {
+//    static var firstName = [ "Алексей", "Игорь", "Александр", "Сергей", "John", "Mike", "Stewart", "Michel", "Sara", "Alina" ]
+//    static var secondName = [ "Навальны", "Байдак", "Стрельцов", "Сизый", "Newmart", "Lesly", "Edwards", "O'bama", "Conor", "Solovieva"]
+//    static var email = [ "night-minsk@mail.ru", "igoraidak@gmail.com", "alinasolo@mail.ru", "qwertt@mail.com", "fkjgnf@gmail.com", "sdni43fd@mail.ru", "dsivsdh@yahoo.com", "cdsfbuw@mail.ru", "djbfiw@mail.by", "cdnfiwejdw@mail.ru"]
+//    static var numberPhone = [ "+37534242423", "+3753232942", "+3754458422932", "+232713423423", "+332234324823", "+234232394932", "+43429292343", "+342348298", "+38423230948", "+457282382234"]
+//}
+
+struct Users: UsersProtocol {
+    var firstName: String
+    var secondName: String
+    var email: String
+    var numberPhone: String
+}
